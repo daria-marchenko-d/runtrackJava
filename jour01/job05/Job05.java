6236c73b67aca615e2d6b864bfa38b0c8a80824a
daria-marchenko-d <daria.marchenko@laplateforme.io>
2025-06-23 09:28:56 +0200

    add other files of the first day

---
+++ jour01/job05/Job05.java
@@ -0,0 +1,8 @@
+package jour01.job05;
+
+class Job05{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
---
+++ jour01/job14/Job14.java
@@ -0,0 +1,8 @@
+package jour01.job14;
+
+class Job14 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
